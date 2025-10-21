636c77771f3a2191bedb70cd0a4da89f558dba35
dodii11 <Dodikbima11@gmail.com>
2025-10-21 10:57:39 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PortofolioController;
+use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -13,7 +13,7 @@ Route::get('/', function () {
 });
 
 // ===============================
-// FRONTEND PRODUCT ROUTES
+// FRONTEND PRODUCT ROUTES (DODI)
 // ===============================
 Route::get('/products', [ProductController::class, 'publicIndex'])->name('frontend.products.index');
 Route::get('/products/{product}', [ProductController::class, 'publicShow'])->name('frontend.products.show');
@@ -24,7 +24,14 @@ Route::prefix('admin')->group(function () {
     // 👇 route khusus ubah status publish
     Route::patch('/products/{product}/publish', [ProductController::class, 'togglePublish'])
         ->name('products.toggle-publish');
-    Route::post('categories', [CategoryController::class, 'store'])->name('categories.store');
 
     Route::resource('portofolio', PortofolioController::class);
+    // 👇 route khusus ubah status publish
+    Route::patch('/portofolio/{portofolio}/publish', [PortofolioController::class, 'togglePublish'])
+        ->name('portofolio.toggle-publish');
+
+    Route::get('/company', [CompanyController::class, 'index'])->name('company.index');
+    Route::post('/company/update', [CompanyController::class, 'update'])->name('company.update');
+
+    Route::post('categories', [CategoryController::class, 'store'])->name('categories.store');
 });
